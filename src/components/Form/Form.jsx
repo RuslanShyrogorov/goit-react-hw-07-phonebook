3b5b58aa56c08c949e15f33dbe1805b44e64a4f4
RuslanShyrogorov <ruslanshirogorov@gmail.com>
2022-09-27 18:04:07 +0300

    add contacts

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { FormContact, FormLabel, FormInput, FormButton } from './Form.styled';
+import { FormContact, FormLabel, FormInput } from './Form.styled';
 import Button from 'components/Button/Button';
 
 export default class Form extends Component {
