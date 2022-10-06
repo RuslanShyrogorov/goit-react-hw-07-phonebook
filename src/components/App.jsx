9a595e3729bf25dc265fc831609616ff44e14966
RuslanShyrogorov <ruslanshirogorov@gmail.com>
2022-10-06 23:22:33 +0300

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import { Box } from './Box';
 import Form from './Form/Form';
@@ -7,36 +7,24 @@ import ContactList from './ContactList/ContactList';
 
 import { MainTitle, Title } from './App.styled';
 
-export default class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
+const initialContacts = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
 
-  componentDidMount() {
-    const contacts = JSON.parse(localStorage.getItem('contacts'));
-    if (contacts?.length) {
-      this.setState({
-        contacts,
-      });
-    }
-  }
+export default function App() {
+  const [contacts, setContacts] = useState(
+    () => JSON.parse(localStorage.getItem('contacts')) ?? initialContacts
+  );
+  const [filter, setFilter] = useState('');
 
-  componentDidUpdate(_, prevState) {
-    const { contacts } = this.state;
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
-    if (prevState.contacts !== contacts) {
-      localStorage.setItem('contacts', JSON.stringify(contacts));
-    }
-  }
-
-  addContact = newContact => {
-    const { contacts } = this.state;
+  const addContact = newContact => {
     const duplicateContact = contacts.find(
       contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
     );
@@ -49,58 +37,46 @@ export default class App extends Component {
       ...newContact,
     };
 
-    this.setState(({ contacts }) => {
-      return {
-        contacts: [contact, ...contacts],
-      };
-    });
+    setContacts(prev => [contact, ...prev]);
   };
 
-  changeFilter = e => {
+  const changeFilter = e => {
     const { value } = e.target;
-    this.setState({ filter: value });
+    setFilter(value);
   };
 
-  filteredContacts = () => {
-    const { contacts, filter } = this.state;
+  const filteredContacts = () => {
     const normalizeFilter = filter.toLowerCase();
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(normalizeFilter)
     );
   };
 
-  deleteContacts = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-    }));
+  const deleteContacts = contactId => {
+    setContacts(prevState =>
+      prevState.filter(contact => contact.id !== contactId)
+    );
     console.log(contactId);
   };
 
-  render() {
-    const { filter } = this.state;
-    const { changeFilter, filteredContacts, deleteContacts, addContact } = this;
-    const visibleContact = filteredContacts();
+  const visibleContact = filteredContacts();
 
-    return (
-      <Box
-        width="30%"
-        mt={3}
-        mb={3}
-        ml={6}
-        p={4}
-        bg="white"
-        borderRadius="normal"
-        boxShadow="card"
-      >
-        <MainTitle>Phonebook</MainTitle>
-        <Form onSubmit={addContact} />
-        <Title>Contacts</Title>
-        <ContactsFilter value={filter} onChange={changeFilter} />
-        <ContactList
-          contacts={visibleContact}
-          onDeleteContact={deleteContacts}
-        />
-      </Box>
-    );
-  }
+  return (
+    <Box
+      width="30%"
+      mt={3}
+      mb={3}
+      ml={6}
+      p={4}
+      bg="white"
+      borderRadius="normal"
+      boxShadow="card"
+    >
+      <MainTitle>Phonebook</MainTitle>
+      <Form onSubmit={addContact} />
+      <Title>Contacts</Title>
+      <ContactsFilter value={filter} onChange={changeFilter} />
+      <ContactList contacts={visibleContact} onDeleteContact={deleteContacts} />
+    </Box>
+  );
 }
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,75 +1,75 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Box } from 'components/Box';
 import { FormLabel, FormInput } from './Form.styled';
 import Button from 'components/Button/Button';
 
-export default class Form extends Component {
-  state = {
-    name: '',
-    number: '',
+import React from 'react';
+
+export default function Form({ onSubmit }) {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handleChange = e => {
+    switch (e.target.name) {
+      case 'name':
+        setName(e.target.value);
+        break;
+      case 'number':
+        setNumber(e.target.value);
+        break;
+      default:
+        return;
+    }
   };
 
-  static propTypes = {
-    onSubmit: PropTypes.func.isRequired,
+  const resetState = () => {
+    setName('');
+    setNumber('');
   };
 
-  handleChange = e => {
-    // console.log(e.target);
-    const { name, value } = e.target;
-    this.setState({ [name]: value });
-  };
-
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    this.props.onSubmit(this.state);
-    this.resetState();
+    onSubmit({ name, number });
+    resetState();
   };
 
-  resetState = () => {
-    this.setState({ name: '', number: '' });
-  };
+  return (
+    <Box
+      border="normal"
+      p={3}
+      mb={5}
+      mt={3}
+      display="flex"
+      flexDirection="column"
+      as="form"
+      onSubmit={handleSubmit}
+    >
+      <FormLabel htmlFor="name">Name</FormLabel>
+      <FormInput
+        type="text"
+        name="name"
+        value={name}
+        id="name"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
+        onChange={handleChange}
+      />
 
-  render() {
-    const { name, number } = this.state;
-    const { handleChange, handleSubmit } = this;
-    return (
-      <Box
-        border="normal"
-        p={3}
-        mb={5}
-        mt={3}
-        display="flex"
-        flexDirection="column"
-        as="form"
-        onSubmit={handleSubmit}
-      >
-        <FormLabel htmlFor="name">Name</FormLabel>
-        <FormInput
-          type="text"
-          name="name"
-          value={name}
-          id="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-          onChange={handleChange}
-        />
+      <FormLabel htmlFor="tel">Number</FormLabel>
+      <FormInput
+        type="tel"
+        name="number"
+        value={number}
+        id="tel"
+        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+        required
+        onChange={handleChange}
+      />
 
-        <FormLabel htmlFor="tel">Number</FormLabel>
-        <FormInput
-          type="tel"
-          name="number"
-          value={number}
-          id="tel"
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-          required
-          onChange={handleChange}
-        />
-
-        <Button type="submit" text="Add contact" />
-      </Box>
-    );
-  }
+      <Button type="submit" text="Add contact" />
+    </Box>
+  );
 }
